1937a9f8d8271e2c790f17715cd7e2ecd873650b
Kadon Liang <kadonliang@gmail.com>
2022-06-14 21:16:07 -0700

    completed task pg func (create, delete, complete)

--- app.py
+++ app.py
@@ -33,31 +33,44 @@ def home():
         return render_template('home.html')
 
 @app.route('/tasks', methods=['GET','POST'])
-def tasks():
+@login_required
+def tasks(uid):
     today = datetime.date.today()
+
     todaySplit = today.strftime("%Y-%m-%d-%H-%M").split("-")
     today = datetime.datetime.strptime(todaySplit[0]+"-"+todaySplit[1]+"-"+todaySplit[2]+"-"+todaySplit[3]+"-"+todaySplit[4], '%Y-%m-%d-%H-%M')
     if request.method == 'GET':
-        tasks = db.tasks.find({'u_id': session['logged_in_id']}).sort([('duetime', pymongo.ASCENDING)])
+        tasks = db.tasks.find({'u_id': session['logged_in_id'], 'complete': False}).sort([('duetime', pymongo.ASCENDING)])
         for task in tasks:
             if task['duetime'] < today: #Past due date
-                db.tasks.update_one({'_id': task['_id']},{
-                '$set': {"late": True}})
-        tasks = db.tasks.find({'u_id': session['logged_in_id']}).sort([('duetime', pymongo.ASCENDING)])
-        return render_template('tasks.html', tasks=tasks, today=todaySplit)
-    elif request.method == "POST":
+                if not(task['late']):
+                    db.tasks.update_one({'_id': task['_id']},{
+                    '$set': {"reward": task['reward']/2}})
+                    db.tasks.update_one({'_id': task['_id']},{
+                    '$set': {"late": True}})
+        tasksList = list(db.tasks.find({'u_id': session['logged_in_id'], 'complete': False}).sort([('duetime', pymongo.ASCENDING)]))
+        for task in tasksList:
+            print("obj",str(task['_id']))
+            task['_id'] = str(task['_id'])
+        print (tasksList)
+        return render_template('tasks.html', tasks=tasksList, today=todaySplit)
+        
+    return render_template('home.html')
+
+@app.route('/addtask', methods=['POST'])
+@login_required
+def addtask(uid):
+    if request.method == 'POST':
         title=request.get_json()['title']
         duetime=request.get_json()['duetime']
         difficulty=request.get_json()['difficulty']
         notes=request.get_json()['notes']
+        reward=(int(difficulty)+1)*2
 
         duetimeSplit = duetime.split("T")
-        print("dtsplit", duetimeSplit)
         duetimeDate = duetimeSplit[0]
         duetimeTime = (duetimeSplit[1]).split(":")
-        print("daaaea",duetimeDate,duetimeTime)
         taskDue = datetime.datetime.strptime(duetimeDate+"-"+duetimeTime[0]+"-"+duetimeTime[1], '%Y-%m-%d-%H-%M') #Saving task due time as yr-mon-day-hr-min
-        print("taskdeu", taskDue)
 
         task = {
             'u_id': session['logged_in_id'],
@@ -65,7 +78,9 @@ def tasks():
             'duetime': taskDue,
             'difficulty': difficulty,
             'notes': notes,
+            'reward': reward,
             'late': False,
+            'complete': False,
         }
 
         tasks = db.tasks
@@ -73,14 +88,46 @@ def tasks():
 
         flash('Task Created', category='success')
         return jsonify({'redirect': '/tasks'})
+    return redirect('/home')
+
+@app.route('/deletetask', methods=['POST'])
+@login_required
+def deletetask(uid):
+    if request.method == 'POST':
+        taskId = request.get_json()['taskID']
+        tasks = db.tasks
+        task = tasks.find_one({'_id': ObjectId(taskId)})
+
+        if task:
+            tasks.delete_one(task)
+            return jsonify({'error': 0})
+
+        return jsonify({'error': 1})
+
+@app.route('/completetask', methods=['POST'])
+@login_required
+def completetask(uid):
+    if request.method == 'POST':
+        taskId = request.get_json()['taskID']
+        task = db.tasks.find_one({'_id': ObjectId(taskId)})
+
+        if task:
+            user = db.users.find_one({'_id': ObjectId(session['logged_in_id'])})
+            if user:
+                db.tasks.update_one(task, {'$set': {'complete': True}})
+                db.users.update_one(user, {'$set': {'balance': user['balance']+task['reward']}})
+                return jsonify({'error': 0})
+            return jsonify({'error': 1})
+        return jsonify({'error': 1})
 
 
 @app.route('/dashboard', methods=['GET','POST'])
-def dashboard():
+@login_required
+def dashboard(uid):
     if request.method == 'GET':
         return render_template('dashboard.html')
 
-@app.route('/login', methods=['GET','POST'])
+@app.route('/login', methods=['POST'])
 def login():
     if request.method == 'POST':
         username=request.get_json()['username']
@@ -102,7 +149,7 @@ def login():
     return redirect('/')
             
         
-@app.route('/signup', methods=['GET','POST'])
+@app.route('/signup', methods=['POST'])
 def signup():
     if request.method == 'POST':
         print(request.json)
@@ -131,7 +178,8 @@ def signup():
     return redirect('/')
 
 @app.route('/logout', methods=['GET','POST'])
-def logout():
+@login_required
+def logout(uid):
     session['logged_in'] = False
     session['logged_in_id'] = ''
     return redirect('/')
